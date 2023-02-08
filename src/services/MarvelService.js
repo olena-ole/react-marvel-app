e88d2bee55e6ea280a556f7009ee56e849372c37
Lenna <pamapm32121@gmail.com>
2023-02-08 21:40:55 +0200

    added MarvelService component to fetch data from api

---
+++ src/services/MarvelService.js
@@ -0,0 +1,25 @@
+
+
+class MarvelService {
+    _apiBase = 'https://gateway.marvel.com:443/v1/public/';
+    _apiKey = 'apikey=4e0edc4fc27a42ebaad59a765f3d4b06'
+    getResource = async (url) => {
+        const res = await fetch(url);
+
+        if (!res.ok) {
+            throw new Error(`Could not fetch ${url}, status: ${res.status}`);
+        }
+
+        return await res.json();
+    }
+
+    getAllCharacters = () => {
+        return this.getResource(`${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`);
+    }
+
+    getCharacter = (id) => {
+        return this.getResource(`${this._apiBase}characters/${id}?limit=9&offset=210&${this._apiKey}`);
+    }
+}
+
+export default MarvelService;

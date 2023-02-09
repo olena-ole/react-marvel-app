5b73c9a073c519d6b5f984964cc8524928f716e0
Lenna <pamapm32121@gmail.com>
2023-02-09 02:09:12 +0200

    added services to receive all characters and a random character from the api, populated RandomChar component

--- src/index.js
+++ src/index.js
@@ -1,17 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/app/App';
-import MarvelService from './services/MarvelService';
 
 import './style/style.scss';
 
-const marvelService = new MarvelService();
-
-marvelService.getAllCharacters(1011096).then(res => console.log(res.data.results[0]));
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <App />
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
 );
 
 

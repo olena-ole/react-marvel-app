63df158c8ba46f0d362e68216a4484ba07ed430a
Lenna <pampam32121@gmail.com>
2023-02-09 19:25:32 +0200

    data for randomChar component is fetched, the component is updated on Try It btn click, the dummy image is centered

--- src/components/randomChar/RandomChar.js
+++ src/components/randomChar/RandomChar.js
@@ -7,10 +7,6 @@ import './randomChar.scss';
 import mjolnir from '../../resources/img/mjolnir.png';
 
 class RandomChar extends Component {
-    constructor(props) {
-        super(props);
-        this.updateChar();
-    }
 
     state = {
         char: {},
@@ -19,6 +15,10 @@ class RandomChar extends Component {
     }
 
     marvelService = new MarvelService();
+
+    componentDidMount() {
+        this.updateChar();
+    }
 
     onCharLoaded = (char) => {
         this.setState({
@@ -61,7 +61,7 @@ class RandomChar extends Component {
                     <p className="randomchar__title">
                         Or choose another one
                     </p>
-                    <button className="button button__main">
+                    <button className="button button__main" onClick={this.updateChar}>
                         <div className="inner">try it</div>
                     </button>
                     <img src={mjolnir} alt="mjolnir" className="randomchar__decoration"/>
@@ -74,9 +74,13 @@ class RandomChar extends Component {
 
 const View = ({char}) => {
     const { name, description, thumbnail, homepage, wiki } = char;
+    const imgStyle = thumbnail.includes('image_not_available') ? 
+                                        {objectFit: 'contain'} : 
+                                        {objectFit: 'cover'};
+
     return (
         <div className="randomchar__block">
-            <img src={thumbnail} alt={name} className="randomchar__img"/>
+            <img src={thumbnail} alt={name} className="randomchar__img" style={imgStyle}/>
             <div className="randomchar__info">
                 <p className="randomchar__name">{name}</p>
                 <p className="randomchar__descr">

b443e168d76bd0880d357b2cbd4ea32ad8db9d75
Lenna <pampam32121@gmail.com>
2023-02-13 02:33:39 +0200

    added methods to download data for the selected character and displayed error/spinner/skeleton depending on state

--- src/components/charInfo/CharInfo.js
+++ src/components/charInfo/CharInfo.js
@@ -1,9 +1,83 @@
+import { Component } from 'react';
+import MarvelService from '../../services/MarvelService';
+
+import Spinner from '../spinner/Spinner';
+import ErrorMessage from '../errorMessage/ErrorMessage';
+import Skeleton from '../skeleton/Skeleton';
+
 import './charInfo.scss';
 import thor from '../../resources/img/thor.jpeg';
 
-const CharInfo = () => {
+class CharInfo extends Component {
+
+    state = {
+        char: null,
+        loading: false,
+        error: false
+    }
+
+    marvelService = new MarvelService();
+
+    componentDidMount() {
+        this.updateChar();
+    }
+
+    
+
+    onCharLoading = () => {
+        this.setState({
+            loading: true,
+            error: false
+        })
+    }
+
+    onCharLoaded = (char) => {
+        this.setState({
+            char, 
+            loading: false,
+        })
+    }
+
+    onError = () => {
+        this.setState({
+            error: true, 
+            loading: false
+        })
+    }
+
+    updateChar = () => {
+        const { charId } = this.props;
+        if (!charId) {
+            return;
+        }
+        this.onCharLoading();
+        this.marvelService
+            .getCharacter(charId)
+            .then(this.onCharLoaded)
+            .catch(this.onError);
+    };
+
+    render() {
+        const { char, loading, error } = this.state;
+        const skeleton = (char || loading || error) ? null : <Skeleton />;
+        const errorMessage = error ? <ErrorMessage /> : null;
+        const spinner = loading ? <Spinner /> : null;
+        const content = !(loading || error || !char) ? <View char={char} /> : null;
+
+        return (
+            <div className="char__info">
+                {skeleton}
+                {errorMessage}
+                {spinner}
+                {content}
+            </div>
+        )
+    }
+}
+
+const View = ({char}) => {
     return (
-        <div className="char__info">
+        <>
             <div className="char__basics">
                 <img src={thor} alt="abyss"/>
                 <div>
@@ -54,7 +128,7 @@ const CharInfo = () => {
                     Avengers (1996) #1
                 </li>
             </ul>
-        </div>
+        </>
     )
 }
 

d80f3196db1a36467de4e8149d66af6a3de3f8e8
Lenna <pampam32121@gmail.com>
2023-02-19 22:54:36 +0200

    added ComicsList component

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -4,6 +4,7 @@ import AppHeader from "../appHeader/AppHeader";
 import RandomChar from "../randomChar/RandomChar";
 import CharList from "../charList/CharList";
 import CharInfo from "../charInfo/CharInfo";
+import ComicsList from "../comicsList/ComicsList";
 
 import decoration from '../../resources/img/vision.png';
 
@@ -19,7 +20,7 @@ const App = () => {
         <div className="app">
             <AppHeader/>
             <main>
-                <ErrorBoundary>
+                {/* <ErrorBoundary>
                     <RandomChar/>
                 </ErrorBoundary>
                 <div className="char__content">
@@ -29,8 +30,11 @@ const App = () => {
                     <ErrorBoundary>
                         <CharInfo charId={selectedChar} />
                     </ErrorBoundary>
-                </div>
-                <img className="bg-decoration" src={decoration} alt="vision"/>
+                </div>        
+                <img className="bg-decoration" src={decoration} alt="vision"/> */}
+                <ErrorBoundary>
+                    <ComicsList />
+                </ErrorBoundary>
             </main>
         </div>
     )

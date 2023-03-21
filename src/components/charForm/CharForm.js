0aea68288052d09813b71cb3bdd10cb0b67fe4c8
Lenna <pampam32121@gmail.com>
2023-03-21 17:06:52 +0200

    updated form with Formik, Yup, showing errors if nothing found

--- src/components/charForm/CharForm.js
+++ src/components/charForm/CharForm.js
@@ -1,35 +1,55 @@
 import { useState } from 'react';
 import useMarvelService from '../../services/MarvelService';
+import { Formik, Form, Field, ErrorMessage as FormikErrorMessage} from 'formik';
+import * as Yup from 'yup';
+import ErrorMessage from '../errorMessage/ErrorMessage';
 
 import './charFrom.scss';
 
 const CharFrom = () => {
-    const [name, setName] = useState('');
-    const { loading, error, clearError, getCharacterByName } = useMarvelService();
+    const [char, setChar] = useState(null);
+    const { error, clearError, getCharacterByName } = useMarvelService();
 
-    function handleChange(e) {
-        e.preventDefault();
-        getCharacterByName(name).then(res => console.log(res));
+    function updateChar(name) {
+        clearError();
+
+        getCharacterByName(name).then(res => setChar(res));
     }
 
     return (
-        <div className="char__search">
-            <p className="char__search-name">Or find a character by name:</p>
-            <form className="char__form" onSubmit={handleChange}>
-                <input 
-                    type="text" 
-                    name="name" 
-                    placeholder="Enter name" 
-                    className="char__form-item" 
-                    required
-                    value={name}
-                    onChange={e => setName(e.target.value)} />
-                <button className="button button__main">
-                    <div className="inner">Find</div>
-                </button>
-                {error && <div>error</div>}
-            </form>
-        </div>
+
+            <Formik
+                initialValues={{ name: '' }}
+                validationSchema={Yup.object({
+                    name: Yup.string()
+                            .min(2, 'The name is too short')
+                            .required('The field is required')
+                })}
+                onSubmit={(values, actions) => {
+                    updateChar(values.name);
+                    actions.setSubmitting(false);
+                    actions.resetForm();
+                }}
+                >
+                {({ isSubmitting }) => (
+                    <div className="char__search">
+                        <p className="char__search-name">Or find a character by name:</p>
+                        <Form className="char__form">
+                            <Field 
+                                type="text" 
+                                name="name" 
+                                placeholder="Enter name" 
+                                className="char__form-item"/>
+                            <button type="submit" disabled={isSubmitting} className="button button__main">
+                                <div className="inner">Find</div>
+                            </button>
+                        </Form>
+                        <FormikErrorMessage name="name" component="div" className="char__search-error" />
+                        {char && char.length && !error ? <div className="char__search-error">{char}</div> : null}
+                        {error ? <div className="char__search-critical-error"><ErrorMessage /></div> : null}
+                    </div>
+                )}
+            </Formik>
     )
 }
 

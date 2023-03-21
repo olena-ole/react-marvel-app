ea9efde8a095f4e2e6b8ddd1e901eeff060e8b0c
Lenna <pampam32121@gmail.com>
2023-03-21 17:24:42 +0200

    displayed successful result in form

--- src/components/charForm/CharForm.js
+++ src/components/charForm/CharForm.js
@@ -15,41 +15,50 @@ const CharFrom = () => {
 
         getCharacterByName(name).then(res => setChar(res));
     }
+    
+    const result = char && char.name && (
+        <div className="char__form">
+            <div className="char__search-success">There is! Visit {char.name} page?</div>
+            <button className="button button__secondary">
+                <div className="inner">To page</div>
+            </button>
+        </div>
+    )
 
     return (
-
-            <Formik
-                initialValues={{ name: '' }}
-                validationSchema={Yup.object({
-                    name: Yup.string()
-                            .min(2, 'The name is too short')
-                            .required('The field is required')
-                })}
-                onSubmit={(values, actions) => {
-                    updateChar(values.name);
-                    actions.setSubmitting(false);
-                    actions.resetForm();
-                }}
-                >
-                {({ isSubmitting }) => (
-                    <div className="char__search">
-                        <p className="char__search-name">Or find a character by name:</p>
-                        <Form className="char__form">
-                            <Field 
-                                type="text" 
-                                name="name" 
-                                placeholder="Enter name" 
-                                className="char__form-item"/>
-                            <button type="submit" disabled={isSubmitting} className="button button__main">
-                                <div className="inner">Find</div>
-                            </button>
-                        </Form>
-                        <FormikErrorMessage name="name" component="div" className="char__search-error" />
-                        {char && char.length && !error ? <div className="char__search-error">{char}</div> : null}
-                        {error ? <div className="char__search-critical-error"><ErrorMessage /></div> : null}
-                    </div>
-                )}
-            </Formik>
+        <Formik
+            initialValues={{ name: '' }}
+            validationSchema={Yup.object({
+                name: Yup.string()
+                        .min(2, 'The name is too short')
+                        .required('The field is required')
+            })}
+            onSubmit={(values, actions) => {
+                updateChar(values.name);
+                actions.setSubmitting(false);
+                actions.resetForm();
+            }}
+            >
+            {({ isSubmitting }) => (
+                <div className="char__search">
+                    <p className="char__search-name">Or find a character by name:</p>
+                    <Form className="char__form">
+                        <Field 
+                            type="text" 
+                            name="name" 
+                            placeholder="Enter name" 
+                            className="char__form-item"/>
+                        <button type="submit" disabled={isSubmitting} className="button button__main">
+                            <div className="inner">Find</div>
+                        </button>
+                    </Form>
+                    <FormikErrorMessage name="name" component="div" className="char__search-error" />
+                    {char && char.length && !error ? <div className="char__search-error">{char}</div> : null}
+                    {error ? <div className="char__search-critical-error"><ErrorMessage /></div> : null}
+                    {result}
+                </div>
+            )}
+        </Formik>
     )
 }
 

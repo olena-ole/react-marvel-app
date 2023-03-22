70793bcf45857c5e626e55c59d66dd2bb886d62b
Lenna <pampam32121@gmail.com>
2023-03-22 22:54:52 +0200

    added meta tags and title for main, comics, single comics pages using react-helmet

--- src/components/pages/MainPage.js
+++ src/components/pages/MainPage.js
@@ -1,5 +1,5 @@
 import { useState } from "react";
-
+import { Helmet } from "react-helmet";
 import ErrorBoundary from "../errorBoundary/ErrorBoundary";
 import RandomChar from "../randomChar/RandomChar";
 import CharList from "../charList/CharList";
@@ -17,6 +17,13 @@ const MainPage = () => {
 
     return (
         <>
+            <Helmet>
+                <meta
+                    name="description"
+                    content="Marvel information portal"
+                />
+                <title>Marvel information portal</title>
+            </Helmet>
             <ErrorBoundary>
                 <RandomChar/>
             </ErrorBoundary>

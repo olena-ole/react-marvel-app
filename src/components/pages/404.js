9298146e3d9d3681e0945965fd5170aca809a324
Lenna <pampam32121@gmail.com>
2023-03-22 23:03:44 +0200

    added meta tags and title for single character page and 404 page

--- src/components/pages/404.js
+++ src/components/pages/404.js
@@ -1,9 +1,17 @@
 import ErrorMessage from "../errorMessage/ErrorMessage";
 import { Link } from "react-router-dom";
+import { Helmet } from "react-helmet";
 
 const Page404 = () => {
     return (
         <div>
+            <Helmet>
+                <meta
+                    name="description"
+                    content="Something went wrong. The page is not found"
+                />
+                <title>Error</title>
+            </Helmet>
             <ErrorMessage />
             <p style={{'textAlign': 'center', 'fontWeight': 'bold', 'fontSize': '24px'}}>Page doesn't exist</p>
             <Link style={{'display': 'block', 'textAlign': 'center', 'fontWeight': 'bold', 'fontSize': '24px', 'marginTop': '30px'}} to="/">Back to main page</Link>

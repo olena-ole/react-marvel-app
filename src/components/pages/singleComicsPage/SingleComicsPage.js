a4bd9613e1b24c489b5acd6d1017c0a3f1a5250b
Lenna <pampam32121@gmail.com>
2023-03-22 22:38:47 +0200

    deleted comments

--- src/components/pages/singleComicsPage/SingleComicsPage.js
+++ src/components/pages/singleComicsPage/SingleComicsPage.js
@@ -1,10 +1,4 @@
 import { Link } from 'react-router-dom';
-// import { useState, useEffect } from 'react';
-
-// import useMarvelService from '../../../services/MarvelService';
-// import Spinner from '../../spinner/Spinner';
-// import ErrorMessage from '../../errorMessage/ErrorMessage';
-// import AppBanner from '../../appBanner/AppBanner';
 
 import './singleComicsPage.scss';
 
@@ -24,55 +18,6 @@ const SingleComicsPage = ({data}) => {
             <Link to="/comics" className="single-comic__back">Back to all</Link>
         </div>
     )
-    // const { id } = useParams();
-    // const [comics, setComics] = useState(null);
-    // const {loading, error, getComics, clearError} = useMarvelService();
-
-    // useEffect(() => {
-    //     updateComics();
-    //     // eslint-disable-next-line
-    // }, [id]);
-
-
-    // const onComicsLoaded = (newComics) => {
-    //     setComics(newComics);
-    // }
-
-    // const updateComics = () => {
-    //     clearError();
-    //     getComics(id)
-    //         .then(onComicsLoaded)
-    // }
-
-    // const errorMessage = error ? <ErrorMessage /> : null;
-    // const spinner = loading ? <Spinner /> : null;
-    // const content = !(loading || error || !comics) ? <View comics={comics} /> : null;
-
-    // return (
-    //     <>
-    //         <AppBanner />
-    //         {errorMessage}
-    //         {spinner}
-    //         {content}
-    //     </>
-    // )
 }
-
-// const View = ({comics}) => {
-//     const {title, description, pageCount, thumbnail, price, language} = comics;
-//     return (
-//         <div className="single-comic">
-//             <img src={thumbnail} alt={title} className="single-comic__img"/>
-//             <div className="single-comic__info">
-//                 <h2 className="single-comic__name">{title}</h2>
-//                 <p className="single-comic__descr">{description}</p>
-//                 <p className="single-comic__descr">{pageCount}</p>
-//                 <p className="single-comic__descr">Language: {language}</p>
-//                 <div className="single-comic__price">{price}</div>
-//             </div>
-//             <Link to="/comics" className="single-comic__back">Back to all</Link>
-//         </div>
-//     )
-// }
 
 export default SingleComicsPage;
